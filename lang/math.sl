503411f363dd2e5442a357162ec07d087feed157
unknown <joeljobinse@gmail.com>
2026-02-16 18:04:21 +0530

    rename lang to quirk for deployment

--- lang/math.sl
+++
@@ -1,5 +0,0 @@
-function sqrt(x)
-    return x ** 0.5
-end
-
-pi = 3.14159
--- lang/run.py
+++
@@ -1,9 +0,0 @@
-from mylang import Interpreter, RuntimeAbort
-
-with open("test.my") as f:
-    code = f.read()
-
-try:
-    Interpreter().run(code)
-except RuntimeAbort as e:
-    print("Runtime Error:", e.message)
